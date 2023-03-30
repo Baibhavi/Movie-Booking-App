29246e36bf24742dbe5276d0b00be1cb4b327f08
Baibhavi <baibhavipandey@gmail.com>
2023-03-30 21:19:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require ("express");
+const mongoose  = require ("mongoose");
+const app = express();
+const dotenv  = require ('dotenv');
+const userRouter  = require ("./routes/userRoutes.js");
+const adminRouter  = require  ("./routes/adminRoutes.js");
+const movieRouter  = require  ("./routes/movieRoutes.js");
+const bookingRouter = require("./routes/bookingRoutes.js");
+const cors = require('cors');
+app.use(cors());
+dotenv.config();
+const PORT= process.env.PORT;
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
+//middleware routes
+app.use(express.json());
+app.use("/user", userRouter); 
+app.use("/admin", adminRouter); 
+app.use("/movie", movieRouter);
+app.use("/booking", bookingRouter);
+
+//http://localhost3000/admin/signup
+//Body: email+password
+
+const pass = process.env.MONGODB_PASSWORD ;
+mongoose.connect('mongodb+srv://Baibhavi-Pandey:'+pass+'@movie-booking.jkcbiek.mongodb.net/bookings?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true,}
+)
+.then(() => 
+        console.log("connected successfully")
+)
+    .catch((e) => console.log(e));
+
+    app.listen(PORT,()=>{
+        console.log(`SERVER RUNNING ON ${PORT}`);
+    })
